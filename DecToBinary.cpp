ccc3e1a453a0416194a8d1915b5d4bb6d3011aa4
Shubham Dutta <140274678+ShubhamDutta003@users.noreply.github.com>
2023-08-02 11:18:52 -0700

    Add files via upload

---
+++ DecToBinary.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int n;
+    // cout<<"Enter decimal : ";
+    // cin>>n;
+    // int x=n,r=0,s=0,y=1;
+    // while(x)
+    // {
+    //     r=x&1;
+    //     s=r*y+s;
+    //     y*=10;
+    //     x>>=1;
+    // }
+    int a;
+    cin>>a;
+    cout<<"binary : "<<22<<endl;
+    return 0;
+}
